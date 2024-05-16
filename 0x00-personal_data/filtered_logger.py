79537380b96858c01ae3f99814d5194ca271c530
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-05-16 06:32:16 -0700

    add

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,76 +1,14 @@
 #!/usr/bin/env python3
-"""  """
-import os
+""" """
 import re
-import logging
-import mysql.connector
-
-
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(
     fields, redaction: str,
     message: str, separator: str
 ) -> str:
-    """ filter datum function """
+    """ filter_datum function """
     for field in fields:
-        regEx = r'({0}=)[^{1}]*({1})'.format(field, separator)
-        message = re.sub(regEx, r'\1{}\2'.format(redaction), message)
+        pattern = r'({0}=)[^{1}]*({1})'.format(field, separator)
+        message = re.sub(pattern, r'\1{}\2'.format(redaction), message)
     return message
-
-
-class RedactingFormatter(logging.Formatter):
-    """ RedactingFormatter class """
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields):
-        """ """
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record) -> str:
-        """ format function"""
-        log = super(RedactingFormatter, self).format(record=record)
-        return filter_datum(self.fields, self.REDACTION, log, self.SEPARATOR)
-
-
-def get_logger():
-    """ get logger function"""
-    logger = logging.getLogger('user_data')
-    handler = logging.StreamHandler()
-    handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-    logger.addHandler(handler)
-    return logger
-
-
-def get_db():
-    """ get db function """
-    connector = mysql.connector.connect(
-        host=os.getenv('PERSONAL_DATA_DB_HOST'),
-        database=os.getenv('PERSONAL_DATA_DB_NAME'),
-        user=os.getenv('PERSONAL_DATA_DB_USERNAME'),
-        password=os.getenv('PERSONAL_DATA_DB_PASSWORD')
-    )
-    return connector
-
-
-if __name__ == '__main__':
-    db = get_db()
-    logger = get_logger()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users")
-    rows = cursor.fetchall()
-    for row in rows:
-        msg = (
-            "name={}; email={}; phone={}; ssn={}; "
-            "password={}; ip={}; last_login={}; user_agent={};"
-        ).format(
-            row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
-        logger.info(msg)
-    cursor.close()
-    db.close()
