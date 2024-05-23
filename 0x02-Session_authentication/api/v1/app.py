c63e97ce35932e97c71951e716288353243538cb
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-05-23 16:25:18 +0100

    add session cookie method

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -13,22 +13,20 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-
-auth_type = os.environ.get('AUTH_TYPE')
-
-if auth_type == 'basic_auth':
+AUTH_TYPE = os.getenv("AUTH_TYPE")
+if AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif auth_type == 'session_auth':
+elif AUTH_TYPE == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
-elif auth_type == 'session_exp_auth':
+elif AUTH_TYPE == 'session_exp_auth':
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
-elif auth_type == 'session_db_auth':
+elif AUTH_TYPE == 'session_db_auth':
     from api.v1.auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
-elif auth_type == 'auth':
+elif AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
