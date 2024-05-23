12891dd757ed919c6dc5f09de44cb7fe5011cd58
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-05-23 16:07:34 +0100

    add

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" create a class to manage the API authentication """
+from typing import List, TypeVar
+
+
+class Auth:
+    """ auth class """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require auth method """
+        if path is None:
+            return True
+        elif excluded_paths is None or excluded_paths == []:
+            return True
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization header method"""
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        if header is None:
+            return None
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user method"""
+        return None
